ca788c1842fe47540d00086952f510200e5dcb69
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-07-11 11:19:00 +0530

    JAVASCRIPT FULL COURSE

---
+++ Scripts/main.js
@@ -0,0 +1,100 @@
+// console.log("first")
+
+// -----------------------------------------Get User Inputs
+
+/* let yname = prompt("Enter your name","Kasun");
+console.log(yname); */
+
+/* let yname = confirm("Are you Kasun ?");
+console.log(yname); */
+
+// ----------------------------------------- Error
+
+/* console.error('This is an error ')
+console.warn("This is an warn "); */
+
+// ----------------------------------------- Object
+
+/* let a = {
+  name: "Kasun",
+  age: 23,
+  city: "Kandy",
+};
+let b = {
+  name: "Upul",
+  age: 44,
+  city: "Matale",
+};
+let c = {
+  name: "Amal",
+  age: 29,
+  city: "Kegall",
+};
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// console.log({ a, b, c });
+console.table({ a, b, c }); */
+
+// ----------------------------------------- Data types
+
+/* let a = "Dinusha Kasun Heenatiyangala"; //String
+let b = 23; // number
+let c = true; //boolean
+let d = 234n; //bigint
+let e; //undefind
+let f = null; //object
+
+console.log(typeof a)
+console.log(typeof (b))
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f) */
+
+// ----------------------------------------- Convert Data types
+
+/* let a = "Dinusha Kasun Heenatiyangala"; //String
+let b = 23; // number
+let c = true; // boolean
+let d = 234n; // bigint
+let e; // undefind
+let f = null; // object
+let g = "12.322ddd";
+
+// b = String(b);
+// console.log(typeof b);
+
+// c = String(c);
+// console.log(c);
+
+// d = String(d);
+// console.log(d);
+
+// e = String(e);
+// console.log(e);
+
+// f = String(f);
+// console.log(f);
+
+// console.log(Number(c));
+
+// console.log(Number(f));
+
+console.log(parseInt(d));
+
+console.log(parseFloat(g)); */
+
+// ----------------------------------------- Unary
+
+/* let a = -2;
+console.log(a); */
+
+// ----------------------------------------- binart
+
+/* let b = -2 + 5;
+console.log(b);
+ */
