6c1ef236fe3f597b37583586862929667d96113a
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-07-14 22:00:40 +0530

    2024.07.14

--- Scripts/main.js
+++ Scripts/main.js
@@ -93,8 +93,73 @@ console.log(parseFloat(g)); */
 /* let a = -2;
 console.log(a); */
 
-// ----------------------------------------- binart
+// ----------------------------------------- binary
 
 /* let b = -2 + 5;
 console.log(b);
  */
+
+// ----------------------------------------- String methods
+
+/* let a = "Dinusha Kasun Heenatiyangala";
+// console.log(a.substring(2, 6));
+
+// console.log(a.split("a"));
+
+// console.log(a.lastIndexOf("D"));
+
+// console.log(a.charAt("12"));
+
+// console.log(a.endsWith("a"));
+
+// console.log(a.replace("Kasun","Diu")); 
+
+//console.log(a.length); */
+
+// ----------------------------------------- Arrays
+
+// let d = ["Kasun", 12, 21, true, [1, 2, 3], 3.7];
+
+// console.log(d.length)
+
+// console.log(d[4][1])
+
+// console.log(d.lastIndexOf(21))
+
+// console.log(d.reverse());
+
+// let a = "Dinusha Kasun Heenatiyangala".split(" ");
+/*
+console.log(a);
+a[1] = "din";
+console.log(d.concat(a));
+ */
+
+/* a.unshift("unshift");
+a.push("push");
+console.log(a);
+
+a.shift()
+console.log(a);
+
+a.pop();
+console.log(a); */
+
+// ----------------------------------------- Object
+
+let a = {
+  name: "Dinusha Kasun",
+  age: 23,
+  canFly: true,
+  languages: ["java", "javascript", "c", "c++"],
+  city: {
+    address1: "Kadugnnawa",
+    address2: "Matale",
+  },
+};
+console.log(a["city"]["address1"]);
+
+a.clss = "Javascript full course";
+console.log(a)
+
+
